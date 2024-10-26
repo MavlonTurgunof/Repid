b544311d4f6716f424eb809a4339ad6cf725b603
nodirbek <soliyevnodirbek001@gmail.com>
2024-10-26 01:19:40 -0700

    new code

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,11 @@ import "slick-carousel/slick/slick-theme.css";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-4H11K248F7";
+    script.async = true;
+    document.head.appendChild(script);
+
     window.dataLayer = window.dataLayer || [];
     function gtag() {
       window.dataLayer.push(arguments);
