20c4b811164720148794a4563b81f932736c43f0
jahongirabduazimov707 <jahongirabduazimov707@gmail.com>
2024-10-26 21:55:41 +0500

    Updated

--- pages/services.js
+++ pages/services.js
@@ -1,5 +1,4 @@
 import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import Services from "@/container/OurServices/OurServices";
 import ServicesHeader from "@/container/OurServices/ServicesHeader";
 import ServicesType from "@/container/OurServices/ServicesType";
