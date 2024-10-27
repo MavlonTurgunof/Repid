0446e08dc01fcd78aa98033648baddfcb0aa1721
nodirbek <soliyevnodirbek001@gmail.com>
2024-10-27 09:33:25 -0700

    new code

--- pages/_app.js
+++ pages/_app.js
@@ -23,29 +23,39 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Basic Meta Tags */}
+        {/* HTML Meta Tags */}
         <title>Repid Agency</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Welcome to Repid Agency, providing top-tier services to boost your business."
+        />
 
-        {/* Open Graph Meta Tags for Social Sharing */}
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://repid.uz/" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="Repid Agency" />
         <meta
           property="og:description"
           content="Welcome to Repid Agency, providing top-tier services to boost your business."
         />
-        <meta property="og:image" content="https://www.repid.uz/repid.png" />
-        <meta property="og:url" content="https://www.repid.uz/" />
-        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/images/b1a4cc19-cfd8-42bf-afbe-7c6d08755781.jpg?token=UVywB6mQQK1NOyHt-cSg1xJpM3Zd0QcbmAFpux2YDaw&height=380&width=864&expires=33266045777"
+        />
 
-        {/* Twitter Card Meta Tags (Optional) */}
+        {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="repid.uz" />
+        <meta property="twitter:url" content="https://repid.uz/" />
         <meta name="twitter:title" content="Repid Agency" />
         <meta
           name="twitter:description"
           content="Welcome to Repid Agency, providing top-tier services to boost your business."
         />
-        <meta name="twitter:image" content="https://www.repid.uz/repid.png" />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/images/b1a4cc19-cfd8-42bf-afbe-7c6d08755781.jpg?token=UVywB6mQQK1NOyHt-cSg1xJpM3Zd0QcbmAFpux2YDaw&height=380&width=864&expires=33266045777"
+        />
       </Head>
 
       <Navbar />
