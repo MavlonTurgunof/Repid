358643dd1cb9a994f794b90e87239bd8113a625f
nodirbek <soliyevnodirbek001@gmail.com>
2024-10-27 09:08:03 -0700

    new code

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,32 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        {/* Basic Meta Tags */}
+        <title>Repid Agency</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* Open Graph Meta Tags for Social Sharing */}
+        <meta property="og:title" content="Repid Agency" />
+        <meta
+          property="og:description"
+          content="Welcome to Repid Agency, providing top-tier services to boost your business."
+        />
+        <meta property="og:image" content="https://www.repid.uz/repid.png" />
+        <meta property="og:url" content="https://www.repid.uz/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card Meta Tags (Optional) */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Repid Agency" />
+        <meta
+          name="twitter:description"
+          content="Welcome to Repid Agency, providing top-tier services to boost your business."
+        />
+        <meta name="twitter:image" content="https://www.repid.uz/repid.png" />
+      </Head>
+
       <Navbar />
       <Component {...pageProps} />
     </>
