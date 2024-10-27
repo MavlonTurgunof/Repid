d72ce75182c536a9b9881fd29ff7386112d0173a
nodirbek <soliyevnodirbek001@gmail.com>
2024-10-27 10:02:09 -0700

    new code

--- pages/about.js
+++ pages/about.js
@@ -9,14 +9,12 @@ function About() {
   return (
     <>
       <Head>
-        {/* HTML Meta Tags */}
         <title>Repid Agency</title>
         <meta
           name="description"
           content="Welcome to Repid Agency, providing top-tier services to boost your business."
         />
 
-        {/* Facebook Meta Tags */}
         <meta property="og:url" content="https://repid.uz/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Repid Agency" />
@@ -29,8 +27,10 @@ function About() {
           content="https://opengraph.b-cdn.net/production/images/b1a4cc19-cfd8-42bf-afbe-7c6d08755781.jpg?token=UVywB6mQQK1NOyHt-cSg1xJpM3Zd0QcbmAFpux2YDaw&height=380&width=864&expires=33266045777"
         />
 
-        {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:card"
+          content="https://opengraph.b-cdn.net/production/images/b1a4cc19-cfd8-42bf-afbe-7c6d08755781.jpg?token=UVywB6mQQK1NOyHt-cSg1xJpM3Zd0QcbmAFpux2YDaw&height=380&width=864&expires=33266045777"
+        />
         <meta property="twitter:domain" content="repid.uz" />
         <meta property="twitter:url" content="https://repid.uz/" />
         <meta name="twitter:title" content="Repid Agency" />
--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,10 @@ function HomePage() {
         />
 
         {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:card"
+          content="https://opengraph.b-cdn.net/production/images/b1a4cc19-cfd8-42bf-afbe-7c6d08755781.jpg?token=UVywB6mQQK1NOyHt-cSg1xJpM3Zd0QcbmAFpux2YDaw&height=380&width=864&expires=33266045777"
+        />
         <meta property="twitter:domain" content="repid.uz" />
         <meta property="twitter:url" content="https://repid.uz/" />
         <meta name="twitter:title" content="Repid Agency" />
