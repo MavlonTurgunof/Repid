bceb1cea523be77e5f2920b492a7cce73ef48d0a
nodirbek <soliyevnodirbek001@gmail.com>
2024-10-27 09:08:28 -0700

    new code

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "@/styles/global.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
