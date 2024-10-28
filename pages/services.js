b0fc44bd909ab6626b3fda2d1140e3350d7b63f2
nodirbek <soliyevnodirbek001@gmail.com>
2024-10-27 22:40:42 -0700

    new code

--- pages/services.js
+++ pages/services.js
@@ -3,15 +3,53 @@ import Services from "@/container/OurServices/OurServices";
 import ServicesHeader from "@/container/OurServices/ServicesHeader";
 import ServicesType from "@/container/HomePage/Services";
 import React from "react";
+import Head from "next/head";
 
 function OurServices() {
   return (
-    <div>
-      <ServicesHeader />
-      <Services />
-      <ServicesType />
-      <Footer />
-    </div>
+    <>
+     <Head>
+        <title>Repid Agency</title>
+        <meta
+          name="description"
+          content="Welcome to Repid Agency, providing top-tier services to boost your business."
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://repid.uz/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Repid Agency" />
+        <meta
+          property="og:description"
+          content="Welcome to Repid Agency, providing top-tier services to boost your business."
+        />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/images/b1a4cc19-cfd8-42bf-afbe-7c6d08755781.jpg?token=UVywB6mQQK1NOyHt-cSg1xJpM3Zd0QcbmAFpux2YDaw&height=380&width=864&expires=33266045777"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="repid.uz" />
+        <meta property="twitter:url" content="https://repid.uz/" />
+        <meta name="twitter:title" content="Repid Agency" />
+        <meta
+          name="twitter:description"
+          content="Welcome to Repid Agency, providing top-tier services to boost your business."
+        />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/images/b1a4cc19-cfd8-42bf-afbe-7c6d08755781.jpg?token=UVywB6mQQK1NOyHt-cSg1xJpM3Zd0QcbmAFpux2YDaw&height=380&width=864&expires=33266045777"
+        />
+      </Head>
+
+      <div>
+        <ServicesHeader />
+        <Services />
+        <ServicesType />
+        <Footer />
+      </div>
+    </>
   );
 }
 
